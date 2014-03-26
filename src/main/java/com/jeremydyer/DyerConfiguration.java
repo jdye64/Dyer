3e6b4b8a4cb75a4db803516d188d0315928523fc
jdye64 <jdye64@gmail.com>
2014-03-26 11:41:51 -0400

    Database configuration added

--- src/main/java/com/jeremydyer/DyerConfiguration.java
+++ src/main/java/com/jeremydyer/DyerConfiguration.java
@@ -2,7 +2,10 @@ package com.jeremydyer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
-import org.hibernate.validator.constraints.NotEmpty;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * User: Jeremy Dyer
@@ -12,29 +15,12 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class DyerConfiguration
     extends Configuration {
 
-    @NotEmpty
-    private String template;
-
-    @NotEmpty
-    private String defaultName = "Stranger";
-
+    @Valid
+    @NotNull
     @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
+    private DataSourceFactory database = new DataSourceFactory();
 
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
     }
 }
