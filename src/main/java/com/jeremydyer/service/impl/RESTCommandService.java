9916865d325a70d59e1fca98accf12097b4391a4
jdye64 <jdye64@gmail.com>
2014-03-31 10:48:21 -0400

    Updated for commands to be executed on the remote server.

--- src/main/java/com/jeremydyer/service/impl/RESTCommandService.java
+++ src/main/java/com/jeremydyer/service/impl/RESTCommandService.java
@@ -1,8 +1,16 @@
 package com.jeremydyer.service.impl;
 
+import com.jeremydyer.core.NetworkDevice;
+import com.jeremydyer.core.NetworkDeviceService;
+import com.jeremydyer.core.NetworkDeviceServiceCommand;
 import com.jeremydyer.core.dto.NetworkServiceCommandResponseDTO;
+import com.jeremydyer.dao.NetworkDeviceDao;
 import com.jeremydyer.dao.NetworkDeviceServiceCommandDao;
+import com.jeremydyer.dao.NetworkDeviceServiceDao;
 import com.jeremydyer.service.CommandService;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,15 +27,69 @@ public class RESTCommandService
     private static final Logger logger = LoggerFactory.getLogger(RESTCommandService.class);
 
     @Autowired
+    private NetworkDeviceDao networkDeviceDao;
+
+    @Autowired
+    private NetworkDeviceServiceDao networkDeviceServiceDao;
+
+    @Autowired
     private NetworkDeviceServiceCommandDao networkDeviceServiceCommandDao;
+
+    private Client client = Client.create();
 
     @Override
     public NetworkServiceCommandResponseDTO executeServiceCommandOnDevice(Long serviceId, Long commandId, Long deviceId) {
         logger.info("About to physically call device.");
 
+        NetworkServiceCommandResponseDTO responseDTO = null;
+
+        NetworkDevice device = networkDeviceDao.find(deviceId);
+        NetworkDeviceService service = networkDeviceServiceDao.find(serviceId);
+        NetworkDeviceServiceCommand command = networkDeviceServiceCommandDao.find(commandId);
+
+        try {
+
+            String commandFullUrl = device.getInternalIpAddress() + ":" + service.getServicePort() + command
+                    .getCommandUri();
+            logger.info("Command Full URL -> '" + commandFullUrl + "'");
+
+            WebResource webResource = client.resource(commandFullUrl);
+            ClientResponse response = null;
+
+            if (command.getHttpMethod().equalsIgnoreCase("GET")) {
+                response = webResource.accept("application/json")
+                        .get(ClientResponse.class);
+            } else if (command.getHttpMethod().equalsIgnoreCase("POST")) {
+                response = webResource.accept("application/json")
+                        .post(ClientResponse.class);
+            } else if (command.getHttpMethod().equalsIgnoreCase("DELETE")) {
+                response = webResource.accept("application/json")
+                        .delete(ClientResponse.class);
+            } else if (command.getHttpMethod().equalsIgnoreCase("PUT")) {
+                response = webResource.accept("application/json")
+                        .put(ClientResponse.class);
+            } else {
+                logger.info("Unsupported HTTPMethod -> '" + command.getHttpMethod() + "'");
+            }
 
 
-        return new NetworkServiceCommandResponseDTO();
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatus());
+            }
+
+            String output = response.getEntity(String.class);
+            logger.info("Output from Server .... \n");
+            logger.info(output);
+
+            responseDTO = new NetworkServiceCommandResponseDTO(device, service, command, output);
+
+        } catch (Exception e) {
+            logger.error("", e);
+            responseDTO = new NetworkServiceCommandResponseDTO(device, service, command, "");
+        }
+
+        return responseDTO;
     }
 
 }
